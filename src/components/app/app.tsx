a856c2e2b650ee91c9aa751012c3261e19979abe
4us4e <145545210+4us4er@users.noreply.github.com>
2025-03-12 23:52:26 +0700

    add: add interface components

---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import { Main } from '../../pages/main/main';
+
+type AppScreenProps = {
+  placesCount: number;
+};
+
+function App({ placesCount }: AppScreenProps): JSX.Element {
+  return <Main placesCount={placesCount} />;
+}
+
+export default App;
