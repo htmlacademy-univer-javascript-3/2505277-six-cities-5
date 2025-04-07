89b7b623ef1e500b92eb42b408ec5df81faae41e
4us4e <145545210+4us4er@users.noreply.github.com>
2025-04-07 23:51:47 +0700

    feat: implement sorting logic for offers

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,5 @@
+import { OfferData } from '../types/offers';
+
 function appendSForPlural(count: number) {
   return count !== 1 ? 's' : '';
 }
@@ -5,4 +7,15 @@ function appendSForPlural(count: number) {
 function getRandomNum(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-export { appendSForPlural, getRandomNum };
+function sortingByType(type: string, offersFiltered: OfferData[]) {
+  const sorted = [...offersFiltered];
+  if (type === 'Price: low to high') {
+    return sorted.sort((a: OfferData, b: OfferData) => a.price - b.price);
+  } else if (type === 'Price: high to low') {
+    return sorted.sort((a: OfferData, b: OfferData) => b.price - a.price);
+  } else if (type === 'Top rated first') {
+    return sorted.sort((a: OfferData, b: OfferData) => b.rating - a.rating);
+  }
+  return offersFiltered;
+}
+export { appendSForPlural, getRandomNum, sortingByType };
